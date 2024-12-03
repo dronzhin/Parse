19da9a01a77862b346671b96ba9b3c7815cc56cc
dmitry <mrakrus25@mail.ru>
2024-12-03 11:39:51 +0500

    Создан бот

--- parse.py
+++ parse.py
@@ -1,39 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from pprint import pprint
 
-main_url = 'https://pythondigest.ru/'
+def parser():
+    main_url = 'https://pythondigest.ru/'
 
-response = requests.get(main_url)
+    response = requests.get(main_url)
 
-if response.status_code == 200:
-    print('Зашли на страницу')
+    result = ''
+    if response.status_code == 200:
+        result += 'Поиск начат\n'
+    else:
+        return 'Нет связи'
 
-soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, 'lxml')
 
-class_news = 'issue issue-end issue-end'
-news_div = soup.find('div', class_=class_news)
-data = []
-new_div_p = news_div.find_all('p')
-for p in new_div_p:
-    url_a = p.a['href']
-    name = list(p.a)[0]
-    response_a = requests.get(url_a)
-    soup_a = BeautifulSoup(response_a.text, 'lxml')
-    lang = soup_a.html['lang']
-    classes = set()
-    count = 0
-    for tag in soup_a.find_all(True):
-        if 'class' in tag.attrs:
+    class_news = 'issue issue-end issue-end'
+    news_div = soup.find('div', class_=class_news)
+    data = []
+    new_div_p = news_div.find_all('p')
+    for p in new_div_p:
+        url_a = p.a['href']
+        name = list(p.a)[0]
+        response_a = requests.get(url_a)
+        soup_a = BeautifulSoup(response_a.text, 'lxml')
+        lang = soup_a.html['lang']
+        classes = set()
+        count = 0
+        for tag in soup_a.find_all(True):
+            if 'class' in tag.attrs:
 
-            classes.update(tag['class'])
-            count += 1
-    data.append([name, url_a, lang, count, classes])
-    print(f'Информация об новости "{name}" добавлена')
+                classes.update(tag['class'])
+                count += 1
+        data.append([name, url_a, lang, count, classes])
+        result += f'Новость - {name} \n Адресс новости - {url_a} \n\n'
+        print(f'Информация об новости "{name}" добавлена')
 
-columns = ['Название новости', 'Ссылка на новость', 'Язык новости', 'Количество классов на сайте', 'Названия классов на сайте']
-df = pd.DataFrame(data, columns=columns)
+    columns = ['Название новости', 'Ссылка на новость', 'Язык новости', 'Количество классов на сайте', 'Названия классов на сайте']
+    df = pd.DataFrame(data, columns=columns)
 
-# Сохранение в CSV
-df.to_csv('data.csv', index=True, encoding='utf-8')
+    # Сохранение в CSV
+    df.to_csv('data.csv', index=True, encoding='utf-8')
+    return result
+
+if __name__ == '__main__':
+    parser()
